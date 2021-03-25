467a6c660bc356b7f4bf848622e2a54bf7b07f25
ColorsWind <14761584+ColorsWind@users.noreply.github.com>
2021-03-25 23:07:31 +0800

    游戏窗体

---
+++ GameWidget.cpp
@@ -0,0 +1,28 @@
+#include "GameWidget.h"
+#include "ui_GameWidget.h"
+#include "Constants.h"
+
+GameWidget::GameWidget(GameBoard* gameBoard, QWidget *parent) :
+    QWidget(parent), gameBoard(gameBoard),
+    ui(new Ui::GameWidget)
+{
+    ui->setupUi(this);
+    gameBoard->setup(this);
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(qDoTick()));
+    timer->start(1000/ Config::fps);
+}
+
+GameWidget::~GameWidget()
+{
+    delete ui;
+}
+
+void GameWidget::mousePressEvent(QMouseEvent *event) {
+    gameBoard->mouseEvent(event->x(), event->y());
+}
+
+void GameWidget::qDoTick() {
+    gameBoard->doTick();
+}
+
