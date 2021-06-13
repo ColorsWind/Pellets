a83e59ad0d15176cce0ce415bca1cf9333d2fd69
ColorsWind <14761584+ColorsWind@users.noreply.github.com>
2021-06-13 17:49:17 +0800

    高Dpi适配

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,11 @@
 #pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
 
 int main(int argc, char *argv[]) {
-    qunsetenv("QT_DEVICE_PIXEL_RATIO");
-    qputenv("QT_SCALE_FACTOR", "2.0");
-    //QApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
 
+    qunsetenv("QT_DEVICE_PIXEL_RATIO");
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+    //QRect deskRect = QApplication::desktop()->availableGeometry();
+    //qputenv("QT_SCALE_FACTOR", "2.0");
     QApplication a(argc, argv);
     MainWindow window(nullptr);
     window.show();
